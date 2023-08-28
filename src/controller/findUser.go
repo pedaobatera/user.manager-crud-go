23db6b56ffeeaf8b11c8d3d1556b7eefc172436a
pedaobatera <padao25@hotmail.com>
2023-08-28 18:16:21 -0300

    Versao 1

---
+++ src/controller/findUser.go
@@ -0,0 +1,15 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func FindUserById(c *gin.Context) {
+	// TODO
+}
+
+func FindAllUsers(c *gin.Context) {
+	// TODO
+}
+
+func FindUsersByEmail(c *gin.Context) {
+	// TODO
+}
---
+++ src/controller/routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pedaobatera/user.manager-crud-go/user.manager-crud-go/src/controller"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+
+	r.GET("/getUserById/:userId", controller.FindUserById)
+	r.GET("/getAllUsers", controller.FindAllUsers)
+	r.GET("/getUsersByEmail/:userEmail", controller.FindUsersByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
+
+}
